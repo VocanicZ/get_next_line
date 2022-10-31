ad8412229a5520e9c9a2a8d3ba6ebfea678e0d66
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-10-31 18:01:11 +0700

    d

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char *str;
+
+    if (argc >= 2)
+    {
+        while (*argv)
+        {
+            printf("path = %s\n", *argv);
+            fd = open(*argv++, O_RDONLY);
+            printf("fd = %d\n", fd);
+        }
+    }
+}
