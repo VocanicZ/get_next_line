207dd3fe665bd6fe0a6bf8cd6c626daf4fa01d63
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-12-24 15:29:53 +0700

    dwa

--- get_next_line_utils3.c
+++ get_next_line_utils3.c
@@ -4,8 +4,9 @@ void    ft_strncpy(char *dest, const char *src, size_t n)
 {
     size_t i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
-    for (; i < n; i++)
-        dest[i] = '\0';
+    i = 0;
+    while (i < n && src[i] != '\0')
+        dest[i] = src[i++];
+    while (i < n)
+        dest[i++] = '\0';
 }
