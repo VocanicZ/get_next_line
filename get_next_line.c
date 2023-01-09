577ce09f0b9b2fe081f9493d7cec692c03371b15
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2023-01-09 15:40:13 +0700

    d

--- get_next_line.c
+++ get_next_line.c
@@ -127,13 +127,13 @@ void	lst_pop2(t_list **lst, t_list **last)
 	if (!lst || !new_node)
 		return ;
 	new_node->next = 0;
-
+	/*
 	i = lst_contains(*last, '\n', 1);
 	printf("i -> %d\n", i);
 	if ((*last)->get && ((*last)->get[i] == '\n'))
 		i++;
 	printf("new i -> %d\n", i);
-
+	*/
 	i = 0;
 	while ((*last)->get[i] && (*last)->get[i] != '\n')
 		i++;
