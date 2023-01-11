36838c0b9ce4e5a9de44e64e34a20eaf112f437d
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2023-01-11 23:51:30 +0700

    cir

--- get_next_line_utils_bonus2.c
+++ get_next_line_utils_bonus2.c
@@ -16,7 +16,6 @@ h_list *lst_get(int fd, h_list **list)
 {
     h_list *current;
 
-    // If the list is empty, create a new node and set it to point to itself
     if (!*list) {
         h_list *new_node = malloc(sizeof(h_list));
         new_node->fd = fd;
@@ -36,10 +35,8 @@ h_list *lst_get(int fd, h_list **list)
         *list = new_node;
         return new_node;
     }
-    // Iterate through the circular buffer
     while (current->next != *list && current->next->fd <= fd) {
         if (current->fd == fd) {
-            // Node with matching fd found, return it
             return current;
         }
         current = current->next;
@@ -86,51 +83,5 @@ void lst_del(int fd, h_list **list)
     if(current->fd == fd) {
         prev->next = *list;
 		free(current);
-    }
-}
-
-void lst_print(h_list *list)
-{
-    h_list *current;
-
-    if (!list) {
-        printf("The list is empty\n");
-        return;
-    }
-    current = list;
-    printf("Circular buffer list: ");
-    while (current->next != list) 
-    {
-        printf("[%d], ", current->fd);
-        current = current->next;
-    }
-    printf("[%d] -> [%d]\n", current->fd, current->next->fd);
-}
-
-void    main(void)
-{
-    static h_list  *list;
-    int     ran;
-    int     i;
-
-    printf("generateing circular\n");
-    srand(time(NULL));
-    i = 0;
-    do
-    {
-        ran = rand() % 100;
-        printf("round %d : rand gen-> %d\n", i, ran);
-        printf("get -> %d\n", lst_get(ran, &list)->fd);
-        lst_print(list);
-        i++;
-    } while (i < 10);
-    while (1)
-    {
-        printf("enter node to delete:");
-        scanf("%d", &ran);
-        printf("del -> %d\n", ran);
-        lst_del(ran, &list);
-        printf("DONE VVV\n");
-        lst_print(list);
     }
 }
