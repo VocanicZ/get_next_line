3e6872411f7cd4f4b16a3952056a8b1c7693bbf2
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2023-01-11 23:02:17 +0700

    cur

--- circular.c
+++ circular.c
@@ -13,27 +13,19 @@ h_list *lst_get(int fd, h_list **list)
         return new_node;
     }
     current = *list;
+    h_list *new_node = malloc(sizeof(h_list));
+    new_node->fd = fd;
     
     // Iterate through the circular buffer
-    while (current->next != *list) {
+    while (current->next != *list && current->fd <= fd) {
         if (current->fd == fd) {
             // Node with matching fd found, return it
             return current;
         }
         current = current->next;
     }
-
-    // If no matching node was found, create a new one
-    h_list *new_node = malloc(sizeof(h_list));
-    new_node->fd = fd;
-
-    // Insert the new node in front of the node with fd < input fd
-    h_list *node = *list;
-    while (node->next != *list && node->next->fd < fd) {
-        node = node->next;
-    }
-    new_node->next = node->next;
-    node->next = new_node;
+    new_node->next = current->next;
+    current->next = new_node;
     return new_node;
 }
 
