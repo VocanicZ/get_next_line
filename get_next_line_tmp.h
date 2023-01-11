abce6e342748a46be64b1bfa60c1880fb6029238
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2023-01-11 14:33:22 +0700

    c

--- get_next_line_tmp.h
+++ get_next_line_tmp.h
@@ -36,10 +36,10 @@ typedef struct	z_list
 char	*get_next_line(int fd);
 int 	ft_strlen(const char *str);
 void	ft_realloc(char **line, t_list *lst);
-void	lst_read(int fd, t_list **lst, t_list **last);
+void	lst_read(int fd, h_list **list);//t_list **lst, t_list **last);
 int 	lst_contains(t_list *lst, char c, int mode);
-void	lst_append(t_list **lst, char *buf, int readed, t_list **last);
+void	lst_append(h_list **list, char *buf, int i);//t_list **lst, char *buf, int readed, t_list **last);
 void	lst_pop(t_list *lst, char **line);
-void	lst_pop2(t_list **lst, t_list **last);
+void	lst_pop2(h_list **list);//t_list **lst, t_list **last);
 void	lst_free(t_list *lst);
 #endif
